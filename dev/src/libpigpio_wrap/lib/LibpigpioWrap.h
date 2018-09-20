db565234c10e5741122abda55ff3945b8eafe7f0
CountrySideEngineer <deep-sleeper@nify.com>
2018-09-20 21:16:59 +0900

    Update library. -Update header file of library, LibpigpioWrap.h. -Update library main file, main.cpp, add function to get library, and access to GPIO, to set mode and change pin value.

--- dev/src/libpigpio_wrap/lib/LibpigpioWrap.h
+++ dev/src/libpigpio_wrap/lib/LibpigpioWrap.h
@@ -20,10 +20,19 @@ extern "C" {
 #define GPIO_INPUT		(0)
 #define GPIO_OUTPUT	(1)
 
+//Result
+#define GPIO_RESULT_ERR_SUCCESS				(0)
+#define GPIO_RESULT_ERR_NOT_INITIALIZED		(1)
+#define GPIO_RESULT_ERR_INVALID_PIN_INDEX		(2)
+#define GPIO_RESULT_ERR_INVALID_ACC_DIRECTION	(3)
+#define GPIO_RESULT_ERR_OUTPUT_VALUE_INVALID	(4)
+#define GPIO_RESULT_ERR_REASON_UNKNOWN			(255)	//0xFF
 
 //Declaration of functions in the library.
 extern uint8_t LibInitialize();
 extern uint8_t LibFinalize();
+extern uint8_t SetGPIOMode(int pin, int mode);
+extern uint8_t SetGPIO(int pin, int value);
 
 #ifdef __cplusplus
 }
--- dev/src/libpigpio_wrap/lib/LibpigpioWrap_main.c
+++ dev/src/libpigpio_wrap/lib/LibpigpioWrap_main.c
@@ -6,6 +6,11 @@
  */
 #include <stdio.h>
 #include "pigpio.h"
+#include "LibpigpioWrap.h"
+
+#define LIB_VERSION_MAJOR		(0xFF)
+#define LIB_VERSION_MINOR		(0x01)
+
 
 static int IsInit = 0;
 
@@ -65,4 +70,90 @@ uint8_t LibFinalize() {
 	return FinalResult;
 }
 
+/**
+ * @brief	Get version of library.
+ * @return	Return library version. This returns 2byte data, upper byte
+ * 			contains major version, lower byte contains minor one.
+ */
+uint16_t LibVersion(void) {
+	uint16_t version = 0;
 
+	version = (((uint16_t)LIB_VERSION_MAJOR) << 8) ||
+			((uint16_t)LIB_VERSION_MINOR);
+
+	return version;
+}
+
+/**
+ * @brief	Configure GPIO pin direction, input or output.
+ * @param[in]	pin		GPIO pin index on RaspberryPi.
+ * @param[in]	mode	Pin access direction, input or output.
+ * 						To configure pin directoin as input, use GPIO_INPUT,
+ * 						as output, use GPIO_OUTPUT.
+ * @return	If pin configuration finished successfully, returns 0, otherwise
+ *			returns none-zero value. The mean of each "none-zero" value is
+ *			below:
+ * 			0x01	:	The library has not been initialized.
+ * 			0x02	:	The pin index is invalid.
+ * 			0x03	:	The mode is invalid.
+ * 			0xFF	:	Unknown error. This value must not been returned.
+ */
+uint8_t SetGPIOMode(int pin, int mode) {
+	int result = GPIO_RESULT_ERR_REASON_UNKNOWN;
+
+	//Check arguments and parameter.
+	if (0 == IsInit) {
+		result = GPIO_RESULT_ERR_NOT_INITIALIZED;
+	} else if ((pin < 0) || (53 < pin)) {
+		result = GPIO_RESULT_ERR_INVALID_PIN_INDEX;
+	} else if ((GPIO_OUTPUT != mode) && (GPIO_INPUT != mode)) {
+		result = GPIO_RESULT_ERR_INVALID_ACC_DIRECTION;
+	} else {
+		int ret = gpioSetMode((unsigned int)pin, (unsigned int)mode);
+		if (PI_BAD_GPIO == ret) {
+			result = GPIO_RESULT_ERR_INVALID_PIN_INDEX;
+		} else if (PI_BAD_MODE == ret) {
+			result = GPIO_RESULT_ERR_INVALID_ACC_DIRECTION;
+		} else {
+			result = GPIO_RESULT_ERR_SUCCESS;
+		}
+	}
+	return result;
+}
+
+/**
+ *	@brief	Set PIN value, high and low.
+ *	@param[in]	pin		GPIO pin index on RaspberryPi.
+ *	@param[in]	value	Value to set to value, only GPIO_LV_OFF and GPIO_LV_ON
+ *						are allowed.
+ * @return	If pin configuration finished successfully, returns 0, otherwise
+ *			returns none-zero value. The mean of each "none-zero" value is
+ *			below:
+ * 			0x01	:	The library has not been initialized.
+ * 			0x02	:	The pin index is invalid.
+ * 			0x04	:	The value to set to pin are invalid.
+ * 			0xFF	:	Unknown error. This value must not been returned.
+ */
+uint8_t SetGPIO(int pin, int value) {
+	int result = GPIO_RESULT_ERR_REASON_UNKNOWN;
+
+	//Check arguments and parameter.
+	if (0 == IsInit) {
+		result = GPIO_RESULT_ERR_NOT_INITIALIZED;
+	} else if ((pin < 0) || (53 < pin)) {
+		result = GPIO_RESULT_ERR_INVALID_PIN_INDEX;
+	} else if ((GPIO_LV_OFF != value) && (GPIO_LV_ON != value)) {
+		result = GPIO_RESULT_ERR_OUTPUT_VALUE_INVALID;
+	} else {
+		int ret = gpioWrite((unsigned int)pin, (unsigned int)value);
+		if (PI_BAD_GPIO == ret) {
+			result = GPIO_RESULT_ERR_INVALID_PIN_INDEX;
+		} else if (PI_BAD_LEVEL == ret) {
+			result = GPIO_RESULT_ERR_OUTPUT_VALUE_INVALID;
+		} else {
+			result = GPIO_RESULT_ERR_SUCCESS;
+		}
+	}
+
+	return result;
+}
