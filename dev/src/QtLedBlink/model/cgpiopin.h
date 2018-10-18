6c0118cad63a49890c6e17b96051773a728290f1
CountrySideEngineer <deep-sleeper@nifty.com>
2018-10-18 22:47:09 +0900

    Update code to use pigpio library.

--- dev/src/QtLedBlink/model/cgpiopin.h
+++ dev/src/QtLedBlink/model/cgpiopin.h
@@ -4,12 +4,21 @@
 
 class CGPIOPin
 {
+#ifdef USE_PIGPIO_LIBRARY
+public:
+    enum GPIO_PIN_MODE {
+        GPIO_PIN_MODE_INPUT = 0,
+        GPIO_PIN_MODE_OUTPUT,
+        GPIO_PIN_MODE_MAX
+    };
+#endif  /* USE_PIGPIO_LIBRARY */
+
 public:
     CGPIOPin();
     virtual ~CGPIOPin();
 
     virtual void SetupPinMode(int pin, int mode);
-    virtual void SetPinValue(int value);
+    virtual int SetPinValue(int value);
     virtual int GetPinValue();
 
     int GetPin() { return this->mPin; }
--- dev/src/QtLedBlink/model/ciopin.cpp
+++ dev/src/QtLedBlink/model/ciopin.cpp
@@ -1,10 +1,12 @@
-#include "stdint.h"
+#include <cstdint>
 #include "ciopin.h"
-#include "LibpigpioWrap.h"
+#ifdef USE_PIGPIO_LIBRARY
+#include "pigpio.h"
+#endif  /* USE_PIGPIO_LIBRARY*/
 
 //Initialize static member parameter
 CIOPin* CIOPin::mInstance = nullptr;
-
+bool CIOPin::IsInit = false;
 /**
  * @brief CIOPin::CIOPing constructor
  */
@@ -12,6 +14,21 @@ CIOPin::CIOPin() {
 #ifdef CALL_PIGPIO_LIBRARY
     LibInitialize();
 #endif  /* CALL_PIGPIO_LIBRARY */
+#ifdef USE_PIGPIO_LIBRARY
+    if (true == CIOPin::IsInit) {
+        /*
+         * If the pigpio library has been already initialized,
+         * nothing to do in the constructor.
+         */
+    } else {
+        int InitResult = gpioInitialise();
+        if (InitResult < 0) {
+            CIOPin::IsInit = false;
+        } else {
+            CIOPin::IsInit = true;
+        }
+    }
+#endif  /* USE_PIGPIO_LIBRARY */
 }
 
 /**
@@ -21,4 +38,14 @@ CIOPin::~CIOPin() {
 #ifdef CALL_PIGPIO_LIBRARY
     LibFinalize();
 #endif  /* CALL_PIGPIO_LIBRARY */
+#ifdef USE_PIGPIO_LIBRARY
+    if (true == CIOPin::IsInit) {
+        /*
+         * If the pigpio library has been initialized, successfully,
+         * it must be finalized, otherwise it must be done nothing.
+         */
+        gpioTerminate();
+        CIOPin::IsInit = false;
+    }
+#endif  /* USE_PIGPIO_LIBRARY */
 }
